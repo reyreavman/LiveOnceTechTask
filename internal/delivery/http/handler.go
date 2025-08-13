475e086bf3b2c9282e811cef65b4413505f9fec9
reyreavman <radmirkhurum@gmail.com>
2025-08-13 21:18:57 +0300

    LO 012. Почистил хендлер

--- internal/delivery/http/handler.go
+++ internal/delivery/http/handler.go
@@ -20,7 +20,7 @@ func NewTaskHandler(service task.Service) *TaskHandler {
 	return &TaskHandler{
 		service: service,
 		headers: map[string]string{
-		"Content-Type": "application/json",
+			"Content-Type": "application/json",
 		},
 	}
 }
@@ -32,14 +32,31 @@ func (h *TaskHandler) CreateTask(w http.ResponseWriter, r *http.Request) {
 	var creatTaskRequest models.CreateTaskRequest
 
 	if err := json.NewDecoder(r.Body).Decode(&creatTaskRequest); err != nil {
-		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		error := models.Error{
+			Type:    "invalid request body",
+			Message: err.Error(),
+		}
+		http.Error(
+			w,
+			models.ErrorToJSON(error),
+			http.StatusBadRequest,
+		)
+		return
 	}
 
+	if err := models.ValidateCreateTaskRequest(creatTaskRequest); err != nil {
+		error := models.Error{
+			Type:    "invalid request body",
+			Message: err.Error(),
+		}
+		http.Error(
+			w,
+			models.ErrorToJSON(error),
+			http.StatusBadRequest,
+		)
+		return
+	}
 	taskResponse := h.service.CreateTask(ctx, creatTaskRequest)
-
-	// w.Header().Set("Content-Type", "application/json")
-	// w.WriteHeader(http.StatusCreated)
-	// json.NewEncoder(w).Encode(taskResponse)
 
 	constructResponse(h.headers, http.StatusCreated, w, taskResponse)
 }
@@ -48,22 +65,18 @@ func (h *TaskHandler) GetTaskById(w http.ResponseWriter, r *http.Request) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	taskId := strings.TrimPrefix(r.URL.Path, TASK_BY_ID)
+	taskId := strings.TrimPrefix(r.URL.Path, PATTERN_TASK_BY_ID)
 
 	taskResponse, err := h.service.GetTaskById(ctx, taskId)
 	if err != nil {
 		if errors.Is(err, task.ErrTaskNotFound) {
-			http.Error(w, err.Error(), http.StatusNotFound)
+			w.WriteHeader(http.StatusNotFound)
 			return
 		} else {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 	}
-
-	// w.Header().Set("Content-Type", "application/json")
-	// w.WriteHeader(http.StatusOK)
-	// json.NewEncoder(w).Encode(taskResponse)
 
 	constructResponse(h.headers, http.StatusOK, w, taskResponse)
 }
@@ -74,10 +87,6 @@ func (h *TaskHandler) GetTasks(w http.ResponseWriter, r *http.Request) {
 
 	status := models.Status(r.URL.Query().Get("status"))
 	taskResponse := h.service.GetTasksByStatus(ctx, status)
-	
-	// w.Header().Set("Content-Type", "application/json")
-	// w.WriteHeader(http.StatusOK)
-	// json.NewEncoder(w).Encode(taskResponse)
 
 	constructResponse(h.headers, http.StatusOK, w, taskResponse)
 }
@@ -87,6 +96,6 @@ func constructResponse(headers map[string]string, status int, w http.ResponseWri
 	json.NewEncoder(w).Encode(responseBody)
 
 	for k, v := range headers {
-		w.Header().Set(k ,v)
+		w.Header().Set(k, v)
 	}
 }
